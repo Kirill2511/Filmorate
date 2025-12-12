d0b9a62e31089ed029874a6e8352e8725413669f
N1IKITA2212 <N1IKITA23@yandex.ru>
2025-12-12 18:18:57 +0300

    Добавлена логика работы с режиссерами (#17)

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.service.DirectorService;
+import ru.yandex.practicum.filmorate.validation.OnCreate;
+import ru.yandex.practicum.filmorate.validation.OnUpdate;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/directors")
+@RequiredArgsConstructor
+public class DirectorController {
+    private final DirectorService directorService;
+
+    @GetMapping
+    public List<Director> getAllDirectors() {
+        return directorService.getAllDirectors();
+    }
+
+    @GetMapping("/{id}")
+    public Director getDirectorById(@PathVariable Integer id) {
+        return directorService.getDirectorById(id);
+    }
+
+    @PostMapping
+    public Director addDirector(@Validated(OnCreate.class) @RequestBody Director director) {
+        return directorService.addDirector(director);
+    }
+
+    @PutMapping
+    public Director updateDirector(@Validated(OnUpdate.class) @RequestBody Director director) {
+        return directorService.updateDirector(director);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDirector(@PathVariable Integer id) {
+        directorService.deleteDirector(id);
+    }
+}
